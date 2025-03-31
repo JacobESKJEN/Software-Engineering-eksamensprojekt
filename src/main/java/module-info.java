94cee52e159c1abbd51781bea83b4d4004587d37
Jacob Eskild Jensen <fidilus7@gmail.com>
2025-03-31 14:08:35 +0200

    skabelon-setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module hellofx {
+    requires transitive javafx.controls;
+    requires javafx.fxml;
+
+    opens dtu.projectapp.ui to javafx.fxml; // Gives access to fxml files
+
+    exports dtu.projectapp.ui; // Exports the class inheriting from javafx.application.Application
+}
