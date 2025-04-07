5190b9db6f8f123751530f093143df0abb811b70
Oliver Olsen <oliver@skou-olsen.dk>
2025-04-07 15:52:38 +0200

    Added Shell

--- src/main/java/dtu/projectapp/model/Project.java
+++ src/main/java/dtu/projectapp/model/Project.java
@@ -1,5 +1,7 @@
 package dtu.projectapp.model;
 
+import javax.swing.tree.ExpandVetoException;
+
 public class Project {
     private String name = "";
     private Employee projectLeader;
@@ -22,4 +24,50 @@ public class Project {
     public String getName() {
         return name;
     }
+
+
+    
+    public void getEmployeeStatus(){
+        if (getProjectLeader() == null) {
+            throw new Exception("Project Has No Project Leader!");
+        }
+
+        //project name
+        System.err.println(getName());
+
+        //project leader
+        System.err.println(getProjectLeader());
+
+        
+
+
+
+
+    }
+
+    public void getProjectETA(){
+        if (getProjectLeader() == null) {
+            throw new Exception("Project Has No Project Leader!");
+        }
+
+        //Activity.loggedHours()     (for all activities)
+        //Activity.remainingHours()
+        //Activity.loggedHoursTotal()
+        //Activity.remainingHoursTotal()
+
+
+
+    }
+
+    /**
+     * @details This is the total report. This is a combination of getEmplyeeStatus() and getProjectETA()
+     */
+    public void getProjectReport(){
+        if (getProjectLeader() == null) {
+            throw new Exception("Project Has No Project Leader!");
+        }
+        getEmployeeStatus();
+        getProjectETA();
+    }
+
 }
