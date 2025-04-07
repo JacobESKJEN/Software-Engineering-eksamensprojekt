23b97e7578c95d1e34bd2ca20cdecfb7e3705c8f
Oliver Olsen <oliver@skou-olsen.dk>
2025-04-07 15:55:10 +0200

    add member function and cleanup

--- src/main/java/dtu/projectapp/model/Employee.java
+++ src/main/java/dtu/projectapp/model/Employee.java
@@ -4,18 +4,20 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Employee {
-    private String id;
+    private int id;
     private String credential;
+    private int activitiesAssigned;
 
     // List to store an employee
     private static List<Employee> EmployeeList = new ArrayList<>();
 
-    public Employee(String id, String credential) {
+    public Employee(int id, String credential, int activitiesAssigned) {
         this.id = id;
         this.credential = credential;
+        this.activitiesAssigned = activitiesAssigned;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
@@ -23,11 +25,14 @@ public class Employee {
         return credential;
     }
 
-    // Adds an employee to the list
-    public static void addEmployee (String id, String credential){
-        EmployeeList.add(new Employee(id, credential));
+    public int getActivitiesAssigned(){
+        return activitiesAssigned;
     }
-    
+
+    // Adds an employee to the list
+    public static void addEmployee (int id, String credential, int activitiesAssigned){
+        EmployeeList.add(new Employee(id, credential, activitiesAssigned));
+    }
     
 
 
