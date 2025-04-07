7383cb6e0abee6e536d4ac99f599fce803e9f233
Oliver Olsen <oliver@skou-olsen.dk>
2025-04-07 15:45:39 +0200

    Add emplyee function added

--- src/main/java/dtu/projectapp/model/Employee.java
+++ src/main/java/dtu/projectapp/model/Employee.java
@@ -1,14 +1,34 @@
 package dtu.projectapp.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Employee {
     private String id;
-    private String name;
+    private String credential;
 
-    public Employee(String id) {
+    // List to store an employee
+    private static List<Employee> EmployeeList = new ArrayList<>();
+
+    public Employee(String id, String credential) {
         this.id = id;
+        this.credential = credential;
     }
 
     public String getId() {
         return id;
     }
+
+    public String getCredential(){
+        return credential;
+    }
+
+    // Adds an employee to the list
+    public static void addEmployee (String id, String credential){
+        EmployeeList.add(new Employee(id, credential));
+    }
+    
+    
+
+
 }
